e6ee9384caf35b87a061b70468696c4f0fc6d331
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2023-04-17 22:11:57 +0530

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,13 @@
+#input()
+#variables
+#print()
+
+Program
+*******
+name=input("What's your name?")
+food=input("What's your favorite food?")
+music=input("What's your favorite music?")
+print("Hi ",name,"!")
+print("Your favorite food is ",food)
+print("Your favorite music is ",music)
+print("ThankYou!")
