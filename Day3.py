a75327a5184dcbbe0c27d6d6ce54ce0b881999dd
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2023-04-18 21:54:57 +0530

    Create Day3.py

---
+++ Day3.py
@@ -0,0 +1,6 @@
+food=input("Name a food :")
+plant=input("Nmae a type of plant :")
+cook=input("Name a method of cooking :")
+burn=input("Name a word to describe burned food :")
+item=input("Name a household item :")
+print(cook, food,"with ",burn, plant,"on a bed of ",item)
