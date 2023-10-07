64f2ebbe59a46a0739ae3393c05fbdd2604c0017
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2023-10-07 15:06:23 +0530

    Update Day4.py

--- Day4.py
+++ Day4.py
@@ -1,3 +1,4 @@
+#CodeChef
 Chef is not feeling well today. He measured his body temperature using a thermometer and it came out to be X °F.
 
 A person is said to have fever if his body temperature is strictly greater than 98 °F.
