b5cfe3bd7c4a7b7e5b7824e8e5480df5c3988ce2
Leana Merin Ison <83947709+LEANA18@users.noreply.github.com>
2023-10-07 15:06:04 +0530

    Create Day4.py

---
+++ Day4.py
@@ -0,0 +1,31 @@
+Chef is not feeling well today. He measured his body temperature using a thermometer and it came out to be X °F.
+
+A person is said to have fever if his body temperature is strictly greater than 98 °F.
+
+
+Determine if Chef has fever or not.
+
+
+  Program
+  *******
+  T=int(input())
+for i in range(0,T):
+    X=int(input())
+    if(X>98):
+        print("YES")
+    else:
+        print("NO")
+
+
+Input
+****
+3
+98
+100
+96
+
+Output
+*****
+NO
+YES
+NO
